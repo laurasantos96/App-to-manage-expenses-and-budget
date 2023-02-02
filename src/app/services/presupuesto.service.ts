d0478f4eb555b6020df09d4164456bcd9bfd19b4
Laura Santos <laurasantosvilas96@gmail.com>
2023-02-02 20:45:44 +0100

    app terminada

--- src/app/services/presupuesto.service.ts
+++ src/app/services/presupuesto.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,6 +7,7 @@ import { Injectable } from '@angular/core';
 export class PresupuestoService {
   presupuesto: number;
   restante: number;
+  private gastos$ = new Subject<any>();
 
   constructor() {
     this.presupuesto = 0;
@@ -14,5 +16,10 @@ export class PresupuestoService {
 
    agregarGasto(gasto: any){
     this.restante = this.restante - gasto.cantidad;
+    this.gastos$.next(gasto);
+   }
+
+   getGastos(): Observable<any>{
+    return this.gastos$.asObservable();
    }
 }
